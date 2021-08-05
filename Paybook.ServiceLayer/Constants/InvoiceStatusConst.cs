7dbd22a6e82e5662a019a1c07372e56092d57e95
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2021-08-05 15:24:44 +0530

    Invoice Status Constant Added to Remove Hard Coded Value.

---
+++ Paybook.ServiceLayer/Constants/InvoiceStatusConst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Paybook.ServiceLayer.Constants
+{
+    public class InvoiceStatusConst
+    {
+        public const string Open = "IS_OPEN";
+        public const string Overdue = "IS_OVERDUE";
+        public const string PaidPartial = "IS_PAID_PARTIAL";
+        public const string Paid = "IS_PAID";
+        public const string Close = "IS_CLOSE";
+    }
+}
