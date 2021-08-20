5b7be81ea1f9a6b05661a9484b8c46ecbf3d29fd
Arpit Shrivastava <shrivastava.arpit@hotmail.com>
2021-08-20 13:55:25 +0530

    Dashboard Invoice Count and Payment Count by last 7 days: Query added

---
+++ Paybook.SqlStructureLayer/dbo/Stored Procedures/sps_Dashboard_GetInvoiceCountByDays.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[sps_Dashboard_GetInvoiceCountByDays]
+	@BusinessId INT,
+	@Days INT = 7
+AS
+BEGIN
+	SELECT COUNT(ID) AS [Count] 
+	FROM Invoices
+	WHERE BusinessId = @BusinessId AND IsActive = 1 AND
+		(InvoiceDate BETWEEN DATEADD(DAY, -(@Days), GETDATE()) AND GETDATE())
+END
---
+++ Paybook.SqlStructureLayer/dbo/Stored Procedures/sps_Dashboard_GetPaymentCountByDays.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[sps_Dashboard_GetPaymentCountByDays]
+	@BusinessId INT,
+	@Days INT = 7
+AS
+BEGIN
+	SELECT COUNT(pay.Id) AS [Count] 
+	FROM Payments pay
+		INNER JOIN InvoicePayments ipay ON pay.Id = ipay.PaymentId
+	WHERE pay.BusinessId = @BusinessId AND pay.IsActive = 1 AND
+		(pay.PaymentDate BETWEEN DATEADD(DAY, -(@Days), GETDATE()) AND GETDATE())
+END
